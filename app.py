f21be2bda2238858465ed0a303d4284f036013fc
gustavo <gustavo.vfavero@gmail.com>
2024-01-15 22:31:32 -0300

    style: Improved application flow using if-elif-else statements

--- app.py
+++ app.py
@@ -8,9 +8,20 @@ print("""
       """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
+print('2. Listar restaurantes')
+print('3. Ativar restaurantes')
 print('4. Sair\n')
 
 chose_option = input(f'Escolha um opção: ')
-print('Você escolheu a opção {chose_option}')
+
+if chose_option == 1:
+    print('Cadastrar restaurante')
+
+elif chose_option == 2:
+    print('Listar restaurantes')
+
+elif chose_option == 3:
+    print('Ativar restaurante')
+
+else:
+    print('Encerrando o programa')
