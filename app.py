142a38f90cda26285f659fe38ed3e6e9d678d3e6
gustavo <gustavo.vfavero@gmail.com>
2024-01-15 23:24:56 -0300

    refactor: Organize code into functions for better structure

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-print("""
+def show_program_name():
+      print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -9,28 +10,35 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurantes')
-print('4. Sair\n')
-
-chose_option = int(input(f'Escolha um opção: '))
-# chose_option = int(chose_option)
+def show_options():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurantes')
+    print('4. Sair\n')
 
 def close_app():
     os.system('cls')
     print('Fechando a aplicação\n')
 
+def choose_option():
+      chosen_option = int(input(f'Escolha um opção: '))
 
+      if chosen_option == 1:
+          print('Cadastrar restaurante')
 
-if chose_option == 1:
-    print('Cadastrar restaurante')
+      elif chosen_option == 2:
+          print('Listar restaurantes')
 
-elif chose_option == 2:
-    print('Listar restaurantes')
+      elif chosen_option == 3:
+          print('Ativar restaurante')
 
-elif chose_option == 3:
-    print('Ativar restaurante')
+      else:
+          close_app()
 
-else:
-    close_app()
+def main():
+    show_program_name()
+    show_options()
+    choose_option()
+
+if __name__ == '__main__':
+    main()
