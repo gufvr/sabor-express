b4f0dc6b8a48f5355d958236a13fe6d9ee0f22c5
gustavo <gustavo.vfavero@gmail.com>
2024-01-15 23:07:35 -0300

    feat: Refactor code to use functions for improved readability

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -13,8 +15,13 @@ print('3. Ativar restaurantes')
 print('4. Sair\n')
 
 chose_option = int(input(f'Escolha um opção: '))
-
 # chose_option = int(chose_option)
+
+def close_app():
+    os.system('cls')
+    print('Fechando a aplicação\n')
+
+
 
 if chose_option == 1:
     print('Cadastrar restaurante')
@@ -26,4 +33,4 @@ elif chose_option == 3:
     print('Ativar restaurante')
 
 else:
-    print('Encerrando o programa')
+    close_app()
