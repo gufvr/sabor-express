a4ee6accca8ec85afcb477e1342e0beb25fd1a7d
gustavo <gustavo.vfavero@gmail.com>
2024-01-15 21:59:14 -0300

    feat: Implemented user input and variable storage

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ print('Sabor Express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
-print('4. Sair')
+print('4. Sair\n')
+
+chose_option = input('Escolha um opção: ')
+print('Você escolheu a opção', chose_option)
